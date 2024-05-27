40d414c38f93af267c7f528f68be3f786f5cd9ff
tomosh22 <tomosh22@googlemail.com>
2024-05-27 15:33:49 +0100

    mesh exports

--- premake5.lua
+++ premake5.lua
@@ -222,7 +222,18 @@ project "reactphysics3d"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
+		
+	filter "configurations:Tools_Debug"
+		defines {
+		"VCE_DEBUG",
+		"VCE_TOOLS"
+		}
+		symbols "on"
+	
 
-	filter "configurations:Dist"
-		runtime "Release"
+	filter "configurations:Tools_Release"
+		defines {
+		"VCE_RELEASE",
+		"VCE_TOOLS"
+		}
 		optimize "on"
